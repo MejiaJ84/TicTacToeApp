1c1a962811c731b8e29844cdd81af0994f3854ba
MejiaJ84 <jtmejia1984@gmail.com>
2022-05-05 16:49:48 -0700

    Added a player class to the app to handle player information

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Display
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var player1 = 'X'
-    private var player2 = 'O'
+    // Created a player class to handle player information
+    private val player1 = Player("Player X", "X")
+    private val player2 = Player("Player O", "O")
+
+    // Keeps track of which player's turn it is
     private var activePlayer = player1
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -16,37 +21,49 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Couldnt quite figure out how to hand this as a generic type
+        // or use as a variable
+        findViewById<TextView>(R.id.textView).setText(player1.name)
+
         val reset = findViewById<Button>(R.id.new_game)
+
+        // Creates an array of buttons
         val buttons = arrayOf(
-            findViewById<Button>(R.id.button1),
-            findViewById<Button>(R.id.button2),
-            findViewById<Button>(R.id.button3),
-            findViewById<Button>(R.id.button4),
-            findViewById<Button>(R.id.button5),
-            findViewById<Button>(R.id.button6),
-            findViewById<Button>(R.id.button7),
-            findViewById<Button>(R.id.button8),
+            findViewById(R.id.button1),
+            findViewById(R.id.button2),
+            findViewById(R.id.button3),
+            findViewById(R.id.button4),
+            findViewById(R.id.button5),
+            findViewById(R.id.button6),
+            findViewById(R.id.button7),
+            findViewById(R.id.button8),
             findViewById<Button>(R.id.button9)
         )
 
+
+        //Send the buttons in the array to the onClick function
         for(button in buttons){
             button.setOnClickListener(this)
         }
-
-
-
     }
 
     override fun onClick(v: View?) {
+        // stores the button in the variable btnClicked
         var btnClicked = findViewById<Button>(v!!.id)
 
-        if(btnClicked.text != "X" && btnClicked.text != "O") {
+        // If there is no text present in the button add either an X or an O
+        // and change player, else do nothing
+        if(btnClicked.text != player1.symbol && btnClicked.text != player2.symbol) {
             if (activePlayer.equals(player1)) {
-                btnClicked.setText("X")
+                btnClicked.setText(player1.symbol)
+                findViewById<TextView>(R.id.textView).setText(player2.name)
                 activePlayer = player2
+
             } else {
-                btnClicked.setText("O")
+                btnClicked.setText(player2.symbol)
+                findViewById<TextView>(R.id.textView).setText(player1.name)
                 activePlayer = player1
+
             }
         }
     }
---
+++ app/src/main/java/com/example/tictactoe/Player.kt
@@ -0,0 +1,7 @@
+package com.example.tictactoe
+
+class Player(n: String, s: String) {
+    var name: String = n
+    var symbol: String = s
+}
+
